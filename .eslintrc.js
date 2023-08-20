67cd101ef1cc6c939cfde845c5e0b93bca07d5c6
buddhilanj <buddhilanj@gmail.com>
2023-08-21 07:57:28 +1000

    husky fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-    parser: 'babel-eslint',
-    extends: ['airbnb', 'prettier'],
-    plugins: ['react', 'react-hooks', 'jsx-a11y', 'import', 'prettier'],
-    rules: {
-      'prettier/prettier': ['error'],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      // Add other project-specific rules as needed
-    },
-  };
+  parser: '@babel/eslint-parser',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    // Add other project-specific rules as needed
+  },
+};
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { SafeAreaView, Text} from "react-native";
-import { useSelector } from "react-redux";
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
-export default function HomeScreen(){
-    return <SafeAreaView>
-        <Text>Home</Text>
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
     </SafeAreaView>
+  );
 }
